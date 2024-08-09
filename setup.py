590ab80dea01d7c2239315c4666d30167a20d204
akashagalave <akashagalaveaaa1@gmail.com>
2024-08-09 23:09:10 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Akash Agalave",
+    author_email="akashagalaveaaa1@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
